d807e61a9ebf4608e54f8d19f5c09ed547282d59
sanchit2107 <sanchit.16bcs1184@abes.ac.in>
2019-08-27 23:57:01 +0530

    Adding Recipe List inside recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipiesComponent
+    RecipiesComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule
